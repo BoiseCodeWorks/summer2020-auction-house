b4d790cdab8d546391cc45a1c3731026d1aa30c8
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2020-05-28 11:15:47 -0600

    delete values content

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,9 +1,7 @@
-import ValueSchema from "../models/Value";
 import mongoose from "mongoose";
 import Good from "../models/Good";
 
 class DbContext {
-  Values = mongoose.model("Value", ValueSchema);
   Good = mongoose.model("Good", Good);
 }
 
