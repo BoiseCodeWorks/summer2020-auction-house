2054b52032caba78f37c9894fd0e60af8ac417cf
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2020-05-28 10:50:42 -0600

    Base Goods Service Done

--- server/services/GoodsService.js
+++ server/services/GoodsService.js
@@ -2,20 +2,32 @@ import { dbContext } from "../db/DbContext";
 import { BadRequest } from "../utils/Errors";
 
 class GoodsService {
-    create(body) {
-        throw new Error("Method not implemented.");
+    async find(query = {}) {
+        return await dbContext.Good.find(query)
     }
-    edit(body) {
-        throw new Error("Method not implemented.");
+    async findById(id) {
+        let data = await dbContext.Good.findById(id);
+        if (!data) {
+            throw new BadRequest("Invalid Id")
+        }
+        return data
     }
-    delete(id) {
-        throw new Error("Method not implemented.");
+    async create(body) {
+        return await dbContext.Good.create(body)
     }
-    find(query) {
-        throw new Error("Method not implemented.");
+    async edit(update) {
+        let data = await dbContext.Good.findByIdAndUpdate(update.id, update, { new: true, runValidators: true })
+        if (!data) {
+            throw new BadRequest("Invalid Id")
+        }
+        return data
     }
-    findById(id) {
-        throw new Error("Method not implemented.");
+    async delete(id) {
+        let data = await dbContext.Good.findByIdAndDelete(id)
+        if (!data) {
+            throw new BadRequest("Invalid Id")
+        }
+        return data
     }
 
 }
