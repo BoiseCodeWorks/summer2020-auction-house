ead372ef27f61faab7865312d957c2e49d619c5b
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2020-05-28 10:45:27 -0600

    Base Goods Controller setup

--- server/controllers/GoodsController.js
+++ server/controllers/GoodsController.js
@@ -7,18 +7,48 @@ export class GoodsController extends BaseController {
         super("api/goods");
         this.router
             .get("", this.getAll)
-            .post("", this.create);
+            .get("/:id", this.getById)
+            .post("", this.create)
+            .put("/:id", this.edit)
+            .delete("/:id", this.delete)
     }
-    async getAll(_, res, next) {
+    async getAll(req, res, next) {
         try {
-
+            let data = await goodsService.find(req.query);
+            return res.send(data)
+        } catch (error) {
+            next(error);
+        }
+    }
+    async getById(req, res, next) {
+        try {
+            let data = await goodsService.findById(req.params.id);
+            return res.send(data)
         } catch (error) {
             next(error);
         }
     }
     async create(req, res, next) {
         try {
-
+            let data = await goodsService.create(req.body);
+            return res.send(data)
+        } catch (error) {
+            next(error);
+        }
+    }
+    async edit(req, res, next) {
+        try {
+            req.body.id = req.params.id
+            let data = await goodsService.edit(req.body);
+            return res.send(data)
+        } catch (error) {
+            next(error);
+        }
+    }
+    async delete(req, res, next) {
+        try {
+            let data = await goodsService.delete(req.params.id);
+            return res.send("Delorted")
         } catch (error) {
             next(error);
         }
--- server/services/GoodsService.js
+++ server/services/GoodsService.js
@@ -2,6 +2,21 @@ import { dbContext } from "../db/DbContext";
 import { BadRequest } from "../utils/Errors";
 
 class GoodsService {
+    create(body) {
+        throw new Error("Method not implemented.");
+    }
+    edit(body) {
+        throw new Error("Method not implemented.");
+    }
+    delete(id) {
+        throw new Error("Method not implemented.");
+    }
+    find(query) {
+        throw new Error("Method not implemented.");
+    }
+    findById(id) {
+        throw new Error("Method not implemented.");
+    }
 
 }
 
